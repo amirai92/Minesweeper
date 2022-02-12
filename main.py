6db6b36dd205aa04aaa0f3addbd7d99af4797e0b
אמיר אייזין <amiraizin20@gmail.com>
2022-02-12 21:36:41 +0200

    WIP: added Cell class
    added a dynamic buttons position, added left and right clicks

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utils
 
-
 root = Tk()
-
 root.configure(bg="black")
 root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("Minesweeper Game")
 root.resizable(False, False)
-
 top_frame = Frame(
     root,
-    bg='red',
+    bg='black',
     width=settings.WIDTH,
     height=utils.height_prct(25)
 )
@@ -20,7 +18,7 @@ top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
-    bg='blue',
+    bg='black',
     width=utils.width_prct(25),
     height=utils.height_prct(75)
 )
@@ -28,7 +26,7 @@ left_frame.place(x=0, y=utils.height_prct(25))
 
 center_frame = Frame(
     root,
-    bg='green',
+    bg='black',
     width=utils.width_prct(75),
     height=utils.height_prct(75)
 )
@@ -36,6 +34,14 @@ center_frame.place(
     x=utils.width_prct(25),
     y=utils.height_prct(25),
 )
+
+for x in range(settings.GRID_SIZE):
+    for y in range (settings.GRID_SIZE):
+        c = Cell(x, y)
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(column=x, row=y)
+
+
 
 #Run the window
 root.mainloop()
--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import settings
 
 
-def height_prct(percentage):
+def height_prct(percentage) -> int:
     return (settings.HEIGHT / 100) * percentage
 
-def width_prct(percentage):
+
+def width_prct(percentage) -> int:
     return (settings.WIDTH / 100) * percentage
